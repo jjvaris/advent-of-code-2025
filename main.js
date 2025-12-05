1062b911b2daf4af1d0a3176f4d88e68351f9451
Jouni Varis <jouni.varis@gofore.com>
2025-12-05 10:25:38 +0200

    minor improvement to main

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const day = process.argv[2];
 const inputFile = process.argv[3];
 const { part1, part2 } = await import(`./${day}/solution.js`);
 
-console.log('Part 1:', part1(`${day}/${inputFile}`));
-console.log('Part 2:', part2(`${day}/${inputFile}`));
+const input = `${day}/${inputFile}`;
+console.log('Part 1:', part1(input));
+console.log('Part 2:', part2(input));
