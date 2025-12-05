adf1e29a83646abab97da9fafd883d45159967b0
Jouni Varis <jouni.varis@gofore.com>
2025-12-05 10:27:54 +0200

    improve boilerplate

--- 0/solution.js
+++ 0/solution.js
@@ -2,8 +2,10 @@ import { readRows } from '../advent.js';
 
 export function part1(input) {
   const rows = readRows(input);
+  return 'to be solved';
 }
 
 export function part2(input) {
   const rows = readRows(input);
+  return 'to be solved';
 }
